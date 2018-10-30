1c3024cb83e18a75c76b4a80097e692f20f06d5c
Adam Stinziani <astinziani@mtrx-node06pd.dcm.senecacollege.ca>
2018-10-30 11:45:14 -0400

    ms1

--- A1/MS2/contacts.h
+++ A1/MS2/contacts.h
@@ -1,9 +1,9 @@
 /* -------------------------------------------
-Name:
-Student number:
-Email:
-Section:
-Date:
+Name: ADAM STINZIANI
+Student number: 124521188
+Email: astinziani@myseneca.ca
+Section: IPC144SVV
+Date: 2018-10-30
 ----------------------------------------------
 Assignment: 1
 Milestone:  2
@@ -18,8 +18,18 @@ struct Name {
 
 // Structure type Address declaration
 // Place your code from Milestone #1 here...
-
+struct Address {
+    int streetNum;
+    char street[41];
+    int aptNum;
+    char postalCode[8];
+    char city[41];
+};
 
 // Structure type Numbers declaration
 // Place your code from Milestone #1 here...
-
+struct Numbers {
+    cell[11];
+    home[11];
+    business[11];
+};
