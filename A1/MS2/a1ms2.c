5001c50fb7973b91e7358ec769b433333116c498
Adam Stinziani <astinziani@mtrx-node06pd.dcm.senecacollege.ca>
2018-10-30 15:08:39 -0400

    almost

--- A1/MS2/a1ms2.c
+++ A1/MS2/a1ms2.c
@@ -27,6 +27,7 @@ struct Name fullName = {{0}};
 struct Address fullAddress = {0,{0}};
 struct Numbers fullNumbers = {{0}};
 char y = 'y';
+char Y = 'Y';
 char yn;
 
     // Display the title
@@ -38,7 +39,7 @@ printf("Please enter the contact's first name: ");
 scanf("%30s", fullName.firstName);
 printf("Do you want to enter a middle initial(s)? (y or n): ");
 scanf("%1s", &yn);
-if (yn == y) {
+if (yn == y | yn == Y) {
 	printf("Please enter the contact's middle initial(s): ");
    	scanf("%6s", fullName.middleInitial);
 }
@@ -52,7 +53,7 @@ printf("Please enter the contact's street name: ");
 scanf("%40s", fullAddress.street);
 printf("Do you want to enter an apartment number? (y or n): ");
 scanf("%1s", &yn);
-if (yn == y) {
+if (yn == y | yn == Y) {
 	printf("Please enter the contact's apartment number: ");
 	scanf("%d", fullAddress.aptNum);
 }
@@ -64,25 +65,25 @@ scanf("%40s", fullAddress.city);
     // Contact Numbers Input:
 printf("Do you want to enter a cell phone number? (y or n): ");
 scanf("%1s", &yn);
-if (yn == y) {
+if (yn == y | yn == Y) {
 	printf("Please enter the contact's cell phone number: ");
 	scanf("%d", fullNumbers.cell);
 }
 printf("Do you want to enter a home phone number? (y or n): ");
 scanf("%1s", &yn);
-if (yn == y) {
+if (yn == y | yn == Y) {
 	printf("Please enter the contact's home phone number: ");
 	scanf("%d", fullNumbers.home);
 }
 printf("Do you want to enter a business phone number? (y or n): ");
 scanf("%1s", &yn);
-if (yn == y) {
+if (yn == y | yn == Y) {
 	printf("Please enter the contact's business phone number: ");
 	scanf("%d", fullNumbers.business);
 }
 
     // Display Contact Summary Details
-
+printf("");
 
 
     // Display Completion Message
