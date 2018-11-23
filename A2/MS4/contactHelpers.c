9a8f28443595186c60c1843b338ef22f50ade345
AdamTV <43098728+AdamTV@users.noreply.github.com>
2018-11-23 00:24:35 -0500

    omgeez

--- A2/MS4/contactHelpers.c
+++ A2/MS4/contactHelpers.c
@@ -1,9 +1,9 @@
 /* -------------------------------------------
-Name:           
-Student number: 
-Email:          
-Section:        
-Date:           
+Name:			 ADAM STINZIANI
+Student number:  124521188
+Email:			 astinziani@myseneca.ca
+Section:		 IPC144SVV
+Date:			 2018-11-22
 ----------------------------------------------
 Assignment: 2
 Milestone:  4
@@ -13,84 +13,259 @@ Milestone:  4
 
 #include <stdio.h>
 
-// This source file needs to "know about" the SYSTEM string library functions.
-// HINT: The library name is string.h.
-//       #include the string.h header file on the next line:
+#include <string.h>
 
+#include "contactHelpers.h"
 
-// ----------------------------------------------------------
-// Include your contactHelpers header file on the next line:
+#define MAXCONTACTS 5
 
-
-
-// ----------------------------------------------------------
-// define MAXCONTACTS for sizing contacts array (5):
-
-
-
-//------------------------------------------------------
-// Function Definitions
-//------------------------------------------------------
-
-// +-------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
-// |        function definitions below...            |
-// +-------------------------------------------------+
-
-// clearKeyboard
-
+// clearKeyboard:
+void clearKeyboard(void)
+{
+	while (getchar() != '\n'); // empty execution code block on purpose
+}
 
 // pause:
-
+void pause(void)
+{
+	printf("(Press Enter to Continue)");
+	clearKeyboard();
+}
 
 // getInt:
+int getInt(void)
+{
+	char NL = 'x';
+	int Value;
 
+	while (NL != '\n')
+	{
+		scanf("%d%c", &Value, &NL);
+		if (NL != '\n')
+		{
+			printf("*** INVALID INTEGER *** <Please enter an integer>: ");
+			clearKeyboard();
+		}
+	}
+	return Value;
+}
 
 // getIntInRange:
+int getIntInRange(int min, int max)
+{
+	int Value;
+	Value = getInt();
 
+	while (Value < min || Value > max)
+	{
+		printf("*** OUT OF RANGE *** <Enter a number between %d and %d>: ", min, max);
+		Value = getInt();
+	}
+	return Value;
+}
 
 // yes:
+int yes(void)
+{
+	char Value = 0;
+	char NL = 'x';
 
+	do
+	{
+		scanf("%c%c", &Value, &NL);
+		if ((Value != 'Y' && Value != 'y' && Value != 'N' && Value != 'n') || NL != 10)
+		{
+			clearKeyboard();
+			printf("*** INVALID ENTRY *** <Only (Y)es or (N)o are acceptable>: ");
+		}
+	} while ((Value != 'Y' && Value != 'y' && Value != 'N' && Value != 'n') || NL != 10);
+	if (Value == 'Y' || Value == 'y')
+		return 1;
+	else
+		return 0;
+}
 
 // menu:
+int menu(void)
+{
+	int Value;
 
+	printf("Contact Management System\n");
+	printf("-------------------------\n");
+	printf("1. Display contacts\n");
+	printf("2. Add a contact\n");
+	printf("3. Update a contact\n");
+	printf("4. Delete a contact\n");
+	printf("5. Search contacts by cell phone number\n");
+	printf("6. Sort contacts by cell phone number\n");
+	printf("0. Exit\n\n");
+	printf("Select an option:> ");
+
+	Value = getIntInRange(0, 6);
+
+	return Value;
+}
 
 // contactManagerSystem:
+void contactManagerSystem(void)
+{
+	int Menu = 0, Return = 0;
+	do
+	{
+		Menu = menu();
 
+		switch (Menu) {
+		case 1:
+			printf("\n<<< Feature 1 is unavailable >>>\n\n");
+			pause();
+			printf("\n");
+			break;
+		case 2:
+			printf("\n<<< Feature 2 is unavailable >>>\n\n");
+			pause();
+			printf("\n");
+			break;
+		case 3:
+			printf("\n<<< Feature 3 is unavailable >>>\n\n");
+			pause();
+			printf("\n");
+			break;
+		case 4:
+			printf("\n<<< Feature 4 is unavailable >>>\n\n");
+			pause();
+			printf("\n");
+			break;
+		case 5:
+			printf("\n<<< Feature 5 is unavailable >>>\n\n");
+			pause();
+			printf("\n");
+			break;
+		case 6:
+			printf("\n<<< Feature 6 is unavailable >>>\n\n");
+			pause();
+			printf("\n");
+			break;
+		default:
+			printf("\nExit the program? (Y)es/(N)o: ");
+			Return = yes();
+			printf("\n");
+			if (Return == 1)
+				printf("Contact Management System: terminated\n");
+		}
+	} while (Return != 1);
+}
 
 // +-------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
-// |        empty function definitions below...      |
+// | ====== Assignment 2 | Milestone 3 =======       |
+// +-------------------------------------------------+
+// | Put empty function definitions below...         |
 // +-------------------------------------------------+
 
-// getTenDigitPhone:
-
+// Generic function to get a ten-digit phone number (ensures 10-digit chars entered)
+void getTenDigitPhone(char telNum[])
+{
+	int i, needInput = 1, count, length;
+	while (needInput == 1)
+	{
+		scanf("%s", telNum);
+		clearKeyboard();
+		length = strlen(telNum);
+		count = 0;
+		for (i = 0; i < length && count == 0; i++)
+		{
+			if (telNum[i] < '0' || telNum[i] > '9')
+				count++;
+		}
+		if (length == 10 && count == 0)
+			needInput = 0;
+		else
+			printf("Enter a 10-digit phone number: ");
+	}
+}
 
 // findContactIndex:
-
+int findContactIndex(const struct Contact contacts[], int size, const char cellNum[])
+{
+	int i, Return;
+	for (i = 0; i < size; size++)
+	{
+		Return = strcmp(contacts[size].numbers.cell, cellNum);
+		if (Return == 0)
+			return size;
+	}
+	return -1;
+}
 
 // displayContactHeader
-
+// Put empty function definition below:
+void displayContactHeader(void)
+{
+	printf("+-----------------------------------------------------------------------------+\n");
+	printf("|                              Contacts Listing                               |\n");
+	printf("+-----------------------------------------------------------------------------+\n");
+}
 
 // displayContactFooter
-
+// Put empty function definition below:
+void displayContactFooter(int contacts)
+{
+	printf("+-----------------------------------------------------------------------------+\n");
+	printf("Total contacts: %d\n\n", contacts);
+}
 
 // displayContact:
-
+// Put empty function definition below:
+void displayContact(const struct Contact* contact)
+{
+	printf("%s", contact->name.firstName);
+	if (contact->name.middleInitial[0] != '\0')
+		printf(" %s %s\n", contact->name.middleInitial, contact->name.lastName);
+	else printf(" %s\n", contact->name.lastName);
+	printf("    C: %-10s   H: %-10s   B: %-10s\n", contact->numbers.cell, contact->numbers.home, contact->numbers.business);
+	printf("       %d %s, ", contact->address.streetNumber, contact->address.street);
+	if (contact->address.apartmentNumber > 0)
+		printf("Apt# %d, ", contact->address.apartmentNumber);
+	printf("%s, %s\n" , contact->address.city, contact->address.postalCode);
+}
 
 // displayContacts:
+// Put empty function definition below:
+void displayContacts(const struct Contact contact[], int size)
+{
 
+}
 
 // searchContacts:
+// Put empty function definition below:
+void searchContacts(const struct Contact contact[], int size)
+{
 
+}
 
 // addContact:
+// Put empty function definition below:
+void addContact(struct Contact contact[], int size)
+{
 
+}
 
 // updateContact:
+// Put empty function definition below:
+void updateContact(struct Contact contact[], int size)
+{
 
+}
 
 // deleteContact:
+// Put empty function definition below:
+void deleteContact(struct Contact contact[], int size)
+{
 
+}
 
 // sortContacts:
+// Put empty function definition below:
+void sortContacts(struct Contact contact[], int size)
+{
+
+}
--- A2/MS4/contactHelpers.h
+++ A2/MS4/contactHelpers.h
@@ -1,10 +1,10 @@
-/* ------------------------------------------------------------------
-Name:
-Student number:
-Email:
-Section:
-Date:
----------------------------------------------------------------------
+/* -------------------------------------------
+Name:			 ADAM STINZIANI
+Student number:  124521188
+Email:			 astinziani@myseneca.ca
+Section:		 IPC144SVV
+Date:			 2018-11-21
+----------------------------------------------
 Assignment: 2
 Milestone:  4
 ---------------------------------------------------------------------
@@ -15,68 +15,69 @@ help reduce redundant coding for common repetitive tasks.
 
 // Hint:  This header file now has functions with parameters referring to 
 //        the struct Contact type so be sure to include the contacts.h header:
-
+#include "contacts.h"
 
 //------------------------------------------------------
 // Function Prototypes
 //------------------------------------------------------
 
-// +-------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
-// |        function prototypes here...              |
-// +-------------------------------------------------+
-
 // Clear the standard input buffer
-// clearKeyboard:
-
+void clearKeyboard(void);
 
 // pause:
-
+void pause(void);
 
 // getInt:
-
+int getInt(void);
 
 // getIntInRange:
-
+int getIntInRange(int, int);
 
 // yes:
-
+int yes(void);
 
 // menu:
-
+int menu(void);
 
 // contactManagerSystem:
+void contactManagerSystem(void);
 
+// +-------------------------------------------------+
+// | ====== Assignment 2 | Milestone 3 =======       |
+// +-------------------------------------------------+
+// | NOTE:  Add new function prototypes as per the   |
+// |        Milestone-3 specifications document      |
+// +-------------------------------------------------+
 
-// Generic function to get a ten-digit phone number:
-// getTenDigitPhone:
+// Generic function to get a ten-digit phone number
+void getTenDigitPhone(char[]);
 
+// findContactIndex
+int findContactIndex(const struct Contact[], int, const char[]);
 
-// findContactIndex:
-
-
-// displayContactHeader:
-
+// displayContactHeader
+void displayContactHeader(void);
 
 // displayContactFooter:
-
+void displayContactFooter(int);
 
 // displayContact:
-
+void displayContact(const struct Contact*);
 
 // displayContacts:
-
+void displayContacts(const struct Contact[], int);
 
 // searchContacts:
-
+void searchContacts(const struct Contact[], int);
 
 // addContact:
-
+void addContact(struct Contact[], int);
 
 // updateContact:
-
+void updateContact(struct Contact[], int);
 
 // deleteContact:
-
+void deleteContact(struct Contact[], int);
 
 // sortContacts:
+void sortContacts(struct Contact[], int);
